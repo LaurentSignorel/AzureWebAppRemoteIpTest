c977f30763ba6f88f5057d393cba1b2229510297
Laurent Signorel <laurent.signorel-ext@ubaldi.com>
2021-02-03 15:12:09 +0100

    add solution and WebApp project

---
+++ AzureWebAppRemoteIpTest/AzureWebAppRemoteIpTest.WebApp/Models/RequestPasswordViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureWebAppRemoteIpTest.WebApp.Models
+{
+    public class RequestPasswordViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "L'adresse E-Mail de récupération doit être saisi.")]
+        [EmailAddress(ErrorMessage = "Vous devez saisir une adresse E-Mail valide.")]
+        public string Email { get; set; }
+    }
+}
